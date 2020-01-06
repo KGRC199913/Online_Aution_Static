5a113c1251868c445cccba8db438adb0fe10d33f
Pham Tuan <phamtuan199911@gmail.com>
2020-01-06 15:49:25 +0700

    Near complete Product Details

--- middlewares/view.mdw.js
+++ middlewares/view.mdw.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars');
 const hbs_sections = require('express-handlebars-sections');
 const numeral = require('numeral');
 const moment = require('moment');
+const userModel = require('../models/user.model');
 
 module.exports = function (app) {
   app.engine('hbs', exphbs({
@@ -11,8 +12,27 @@ module.exports = function (app) {
       section: hbs_sections(),
       format: val => numeral(val).format('0,0'),
       nextPrice: (cur, step) => cur + step,
-      getFormatedDateTime: (datetime) => moment(datetime).format('MMMM Do YYYY, h:mm:ss a'),
+      getSimpleDate: (datetime) => moment(datetime).format(`hh:mm Do MMMM YYYY`) ,
+      getFormatedDateTime: (datetime) => moment(datetime).format('Do MMMM YYYY, h:mm:ss a'),
+      shouldDateRelative: (datetime) => {
+        var duration = moment.duration(moment(datetime).diff(moment.now()));
+        return duration.asDays() <= 3;
+      },
+      getRelativeDateTime: (datetime) => moment(datetime).toNow(),
+      isGuest: (user) => !user,
+      isBidder: (user) => user.f_permission === 0,
+      isSeller: (user) => user.f_permission === 1,
+      isAdmin: (user) => user.f_permission === 2,
+      isBidOver: (datetime) => moment(datetime).isBefore(moment.now()),
+      maskString: string => mask(string),
     }
   }));
   app.set('view engine', 'hbs');
 };
+
+function mask(string) {
+  for (let i = 0; i < string.length * 4 / 7 ; ++i) {
+    string = string.substr(0, i) + '*' + string.substr(i + 1);
+  }
+  return string;
+}
