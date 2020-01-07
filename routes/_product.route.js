e056c82712a0fd1fa5fb0425cb8816cf70a6d3c8
shint1001 <48145607+shint1001@users.noreply.github.com>
2020-01-07 17:53:21 +0700

    create Bidding tableDB for Profile 'bidder'

--- routes/_product.route.js
+++ routes/_product.route.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const productModel = require('../models/product.model');
 const userModel = require('../models/user.model');
@@ -9,87 +8,87 @@ const fs = require('fs');
 
 const router = express.Router();
 
-router.get('/byCat/:catId', async function (req, res) {
+router.get('/byCat/:catId', async function(req, res) {
 
-  for (const c of res.locals.lcCategories) {
-    if (c.CatID === +req.params.catId) {
-      c.isActive = true;
+    for (const c of res.locals.lcCategories) {
+        if (c.CatID === +req.params.catId) {
+            c.isActive = true;
+        }
     }
-  }
 
-  let page = +req.query.page || 1;
-  if (page < 0) page = 1;
-  const offset = (page - 1) * config.pagination.limit;
+    let page = +req.query.page || 1;
+    if (page < 0) page = 1;
+    const offset = (page - 1) * config.pagination.limit;
 
-  // const total = await productModel.countByCat(req.params.catId);
+    // const total = await productModel.countByCat(req.params.catId);
 
-  const [total, rows] = await Promise.all([
-    productModel.countByCat(req.params.catId),
-    productModel.pageByCat(req.params.catId, offset)
-  ])
+    const [total, rows] = await Promise.all([
+        productModel.countByCat(req.params.catId),
+        productModel.pageByCat(req.params.catId, offset)
+    ])
 
-  const nPages = Math.ceil(total / config.pagination.limit);
-  const page_items = [];
-  for (i = 1; i <= nPages; i++) {
-    const item = {
-      value: i,
-      isActive: i === page
+    const nPages = Math.ceil(total / config.pagination.limit);
+    const page_items = [];
+    for (i = 1; i <= nPages; i++) {
+        const item = {
+            value: i,
+            isActive: i === page
+        }
+        page_items.push(item);
     }
-    page_items.push(item);
-  }
 
-  // const rows = await productModel.pageByCat(req.params.catId, offset);
-  res.render('vwProducts/byCat', {
-    products: rows,
-    empty: rows.length === 0,
-    page_items,
-    can_go_prev: page > 1,
-    can_go_next: page < nPages,
-    prev_value: page - 1,
-    next_value: page + 1,
-  })
+    // const rows = await productModel.pageByCat(req.params.catId, offset);
+    res.render('vwProducts/byCat', {
+        products: rows,
+        empty: rows.length === 0,
+        page_items,
+        can_go_prev: page > 1,
+        can_go_next: page < nPages,
+        prev_value: page - 1,
+        next_value: page + 1,
+    })
 
 
 
-  // const rows = await productModel.allByCat(req.params.catId);
-  // res.render('vwProducts/byCat', {
-  //   products: rows,
-  //   empty: rows.length === 0
-  // })
+    // const rows = await productModel.allByCat(req.params.catId);
+    // res.render('vwProducts/byCat', {
+    //   products: rows,
+    //   empty: rows.length === 0
+    // })
 })
 
-router.get('/:id', async function (req, res) {
-  const rows = await productModel.single(req.params.id);
-  const seller = await userModel.singleById(rows[0].sellerId);
-  const hgBidder = await userModel.singleById(rows[0].hgBidderId);
+router.get('/:id', async function(req, res) {
+    const rows = await productModel.single(req.params.id);
+    const seller = await userModel.singleById(rows[0].sellerId);
+    const hgBidder = await userModel.singleById(rows[0].hgBidderId);
 
-  const history = await bidHistoryModel.byProId(rows[0].ProID);
-  for (let h in history) {
-    history[h]['user'] = await userModel.singleById(history[h].user_id);
-  }
+    const history = await bidHistoryModel.byProId(rows[0].ProID);
+    for (let h in history) {
+        history[h]['user'] = await userModel.singleById(history[h].user_id);
+    }
 
-  const similar = await productModel.getXRandomProductInCat(5, rows[0].CatID);
+    const similar = await productModel.getXRandomProductInCat(5, rows[0].CatID);
 
-  let imgArr = [];
-  let showImg = [];
-  fs.readdirSync(`./public/imgs/sp/${rows[0].ProID}/`).forEach(file => {
-    if (file.match(/thumb/g)) {
-      imgArr.push(rows[0].ProID + "/" + file);
-    } else {
-      showImg.push(rows[0].ProID + "/" + file);
-    };
-  });
+    let imgArr = [];
+    let showImg = [];
+    fs.readdirSync(`./public/imgs/sp/${rows[0].ProID}/`).forEach(file => {
+        if (file.match(/thumb/g)) {
+            imgArr.push(rows[0].ProID + "/" + file);
+        } else {
+            showImg.push(rows[0].ProID + "/" + file);
+        };
+    });
 
-  res.render('vwProducts/detail', {
-    show: showImg,
-    imgs: imgArr,
-    currentUser: req.session.authUser,
-    history: history,
-    similar: similar,
-    product: rows[0],
-    seller: seller,
-    hgBidder: hgBidder,
-  })
+    res.render('vwProducts/detail', {
+        show: showImg,
+        imgs: imgArr,
+        currentUser: req.session.authUser,
+        history: history,
+        similar: similar,
+        product: rows[0],
+        seller: seller,
+        hgBidder: hgBidder,
+    })
 });
 
 module.exports = router;
