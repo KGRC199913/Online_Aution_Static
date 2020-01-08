72cec1f74f6ee89ed80abff5855ef20558eefa61
DianaSensei <43933486+DianaSensei@users.noreply.github.com>
2020-01-08 09:01:49 +0700

    fix price
    - fix error price can't show

--- middlewares/session.mdw.js
+++ middlewares/session.mdw.js
@@ -14,7 +14,7 @@ module.exports = function(app) {
       host: 'localhost',
       port: 3306,
       user: 'root',
-      password: 'Kuro96',
+      password: '123456',
       database: 'qlbh',
       charset: 'utf8mb4_bin',
       schema: {
--- utils/db.js
+++ utils/db.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
   host: 'localhost',
   port: "3306",
   user: 'root',
-  password: 'Kuro96',
+  password: '123456',
   database: 'qlbh'
 });
 
